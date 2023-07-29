caa83ed3a30b77780da484e4a03e61b5effaef28
joaopedr0sena <joaopedrosena1997@gmail.com>
2023-07-29 15:54:06 -0300

    create: global style

---
+++ styles/global.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const globlaStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  input: {
+    borderColor: '#000',
+    borderWidth: 1,
+  }
+});
